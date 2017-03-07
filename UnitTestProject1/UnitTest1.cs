81db94d94b2910a74d3534b82aafea90a437a74d
Olivier Matis <olivier.matis@equinoxe-Consulting.net>
2017-03-07 21:18:15 +0100

    After Second NDepends

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -14,5 +14,43 @@ namespace UnitTestProject1
 
             Assert.IsFalse(t.MyProperty);
         }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var t = new MyClass();
+
+            var result = t.Method1(5);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var t = new MyClass();
+
+            var result = t.Method1(4);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            var t = new MyClass();
+
+            t.Method2();
+
+            Assert.IsFalse(false);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            var t = new MyClass(true);
+
+            Assert.IsTrue(t.MyProperty);
+        }
     }
 }
